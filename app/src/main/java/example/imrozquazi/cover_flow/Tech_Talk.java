794f8c068f468614ae1fca38a50a5b5234fbfa53
Imroz Quazi <imrozq@gmail.com>
2018-03-15 01:25:38 +0530

    Event Updates

---
+++ app/src/main/java/example/imrozquazi/cover_flow/Tech_Talk.java
@@ -0,0 +1,13 @@
+package example.imrozquazi.cover_flow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tech_Talk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tech__talk);
+    }
+}
